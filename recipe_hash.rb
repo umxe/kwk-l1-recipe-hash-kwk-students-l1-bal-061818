ea2f2a1d31137a815cd63f7eca99853aacb7caa0
umxe <u_habiba@rocketmail.com>
2018-06-21 18:09:16 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -12,28 +12,46 @@
 #  - olive oil - 1/2 cup
 #  - agave - 1/4 cup
 # All of the ingredients should be symbols and the amounts should be strings. Remember that symbols start with a colon and be sure to sub underscores for spaces.
+perfect_10_recipe = {:almond_flour => "2 cups", :gluten_freewhole_oats => "1 cup", :kosher_salt => "1/2 teaspoon", :baking_powder => "1/2 teaspoon", :baking_soda => "1/4 tea spoon", :Xanthan_gum => "1/2 teaspoon", :lightly_toasted_slivered_almonds => "1/4 cup", :mini_dark_chocolate_chips => "3/4 cup", :olive_oil => "1/2 cup", :agave => "1/4 cup"}
 
 
 #2. Write a method that simply returns (not puts) your perfect_10 hash
 def recipe_ingredients(perfect_10_recipe)
+  return perfect_10_recipe
 end
 
 #2: Write code that returns the amount of chocolate chips from perfect_10 ingredients that is needed in the recipe.
 def amount_of_chocolate_chips(perfect_10_recipe)
+  return perfect_10_recipe.value(:mini_dark_chocolate_chips)
 end
 
 #3: Use `puts` to print out every ingredient and amount in the hash by iterating through the collection. You'll want to use the `each` method.
 def ingredients_and_amounts(perfect_10_recipe)
+  perfect_10_recipe.each_pair do |key, value|
+    puts "Ingredient: #{key} Amount: #{value}"
+  end
 end
 
 #4: Now use `puts` to print out just the ingredients without the amounts. You'll want to use the `each_key` method.
 def ingredients(perfect_10_recipe)
+  perfect_10_recipe.each_value do |value|
+    puts "Ingredient: #{value}" 
+    end
 end
 
 #5: Now use `puts` to print out just the amounts without the ingredients. You'll want to use the `each_value` method.
 def amounts(perfect_10_recipe)
+    perfect_10_recipe.each_value do |key|
+    puts "Amount: #{key}"
+    end
 end
 
 #6: Return the number of ingredients in the `perfect_10_recipe` hash.
 def number_ingredients(perfect_10_recipe)
+num =0
+    perfect_10_recipe.each_value do |value|
+    num += 1
+    end
+    return num
 end
+
